f598327d4b455f37c64a4250090ff85b2b702b7c
Snoolf <abdurakhmonovjaloliddin2002@gmail.com>
2024-07-10 18:20:41 +0500

    deleted one url for create user

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import UserView, Users, CreateUser, PostsView, PostView
+from .views import UserView, Users, PostsView, PostView
 
 urlpatterns = [
-    path('users/<int:id>', UserView.as_view()),
+    path('users/<int:id>/', UserView.as_view()),
     path('users/', Users.as_view()),
-    path('users/', CreateUser.as_view()),
     path('posts/', PostsView.as_view()),
-    path('posts/<int:id>', PostView.as_view()),
+    path('posts/<int:id>/', PostView.as_view()),
 ]
