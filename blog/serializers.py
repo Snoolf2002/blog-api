a60cfe23acf1e22965af99d1ef2e69750b6c8bac
Snoolf <abdurakhmonovjaloliddin2002@gmail.com>
2024-07-11 10:08:01 +0500

    add create method to PostSerializer

--- blog/serializers.py
+++ blog/serializers.py
@@ -3,7 +3,12 @@ from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = Post
         fields = '__all__'
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Post` instance, given the validated data.
+        """
+        return Post.objects.create(**validated_data)
